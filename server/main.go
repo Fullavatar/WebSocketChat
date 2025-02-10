53010f26188c363712e7624cd85fd7d423f049de
Fullavatar <59883459+Fullavatar@users.noreply.github.com>
2025-02-10 10:00:17 +0100

    Refactored Project

--- server/main.go
+++ server/main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/fullavatar/websocketchat/websocketserver"
-)
+import websocketserver "github.com/fullavatar/websocketchat/websocketserver/pkg"
 
 func main() {
-	server := websocketserver.NewWebSocketServer()
-
-	http.Handle("/ws", server)
-
-	port := "5000"
-	log.Println("WebSocket server running on ws://localhost:" + port + "/ws")
-
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal("Server error:", err)
-	}
+	websocketserver.OpenWebSocketServer("5000")
 }
